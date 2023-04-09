e8e9ffeb502bdd5ea6f734b2719d8004bc94bfed
Nicolás Deyros <ndeyros@gmail.com>
2023-04-09 20:41:56 -0300

    add vercel.json and shcedule-fetch file

---
+++ src/api/schedule-fetch.js
@@ -0,0 +1,14 @@
+import { fetchCatalogs } from '../fetch-catalogs.mjs'
+import cron from 'node-cron'
+
+export default (req, res) => {
+	// Schedule the fetchCatalogs job to run every day at 11:00 am
+	cron.schedule('0 11 * * *', () => {
+		console.log('Fetching catalogs...')
+		fetchCatalogs().then(() => {
+			console.log('Catalogs updated.')
+		})
+	})
+
+	res.status(200).send('Catalogs fetch scheduled.')
+}
