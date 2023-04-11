1e1e57af010bbf54783a653e2ebd3a938a8110c0
Nicolas Deyros <ndeyros@Lenovo.com>
2023-04-11 14:53:38 -0300

    modify cron.js in order to export the function

--- api/cron.js
+++ api/cron.js
@@ -1,12 +1,14 @@
 import { fetchCatalogs } from './update-catalogs.js'
 import cron from 'node-cron'
 
-cron.schedule('00 12 * * *', async () => {
-	console.log('Updating catalogs...')
-	const result = await fetchCatalogs()
-	if (result.success) {
-		console.log('Catalogs file updated.')
-	} else {
-		console.error(`Error updating catalogs: ${result.error}`)
-	}
-})
+export default function startCronJob() {
+	cron.schedule('00 12 * * *', async () => {
+		console.log('Updating catalogs...')
+		const result = await fetchCatalogs()
+		if (result.success) {
+			console.log('Catalogs file updated.')
+		} else {
+			console.error(`Error updating catalogs: ${result.error}`)
+		}
+	})
+}
