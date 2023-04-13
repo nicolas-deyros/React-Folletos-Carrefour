982e55fbdb30db1c2f49411a559bc4c774387a73
Nicolas Deyros <ndeyros@Lenovo.com>
2023-04-13 00:21:47 -0300

    optimize cron.mjs

--- api/cron.mjs
+++ api/cron.mjs
@@ -5,35 +5,32 @@ import path from 'path'
 
 const filePath = new URL('../tmp/catalogs.json', import.meta.url).pathname
 const now = new Date()
-const date = new Date()
-console.log(
-	`Catalogs fetched at ${date.toLocaleString('en-US', {
-		timeZone: 'America/Argentina/Buenos_Aires',
-	})}`
-)
 
 if (fs.existsSync(filePath)) {
-	console.log('Catalogs file exists. Fetching catalogs...')
-	fetchCatalogs()
-	cron.schedule('00 12 * * *', () => {
-		console.log('Scheduled job: Fetching catalogs...')
+	const stat = fs.statSync(filePath)
+	const mtime = new Date(stat.mtime)
+	const diffHours = (now - mtime) / 1000 / 60 / 60
+
+	if (diffHours >= 24) {
+		console.log(`Catalogs file exists but it's ${diffHours} hours old. Fetching catalogs...`)
 		fetchCatalogs().then(() => {
 			console.log('Catalogs file updated.')
-			process.exit(0)
 		})
-	})
+	} else {
+		console.log(`Catalogs file exists and is less than 24 hours old. Skipping fetch.`)
+	}
 } else {
 	console.log('Catalogs file does not exist. Creating file...')
 	fetchCatalogs().then(() => {
-		console.log('Catalogs file created. Fetching catalogs...')
-		cron.schedule('00 12 * * *', () => {
-			console.log('Scheduled job: Fetching catalogs...')
-			fetchCatalogs().then(() => {
-				console.log('Catalogs file updated.')
-				process.exit(0)
-			})
-		})
+		console.log('Catalogs file created.')
 	})
 }
+
+cron.schedule('00 12 * * *', () => {
+	console.log('Scheduled job: Fetching catalogs...')
+	fetchCatalogs().then(() => {
+		console.log('Catalogs file updated.')
+	})
+})
 
 export default fetchCatalogs
