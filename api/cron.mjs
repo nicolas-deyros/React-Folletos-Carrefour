9405c042b48497aeb71b28e5a210e8d64ce5b077
Nicolas Deyros <ndeyros@Lenovo.com>
2023-04-13 00:18:14 -0300

    update cron.mjs

--- api/cron.mjs
+++ api/cron.mjs
@@ -13,25 +13,25 @@ console.log(
 )
 
 if (fs.existsSync(filePath)) {
-	const stats = fs.statSync(filePath)
-	const ageInMs = now - stats.mtime
-	const ageInHours = ageInMs / (1000 * 60 * 60)
-	if (ageInHours < 24) {
-		console.log('Catalogs file exists and is less than 24 hours old. Skipping fetch.')
-		// process.exit(0)
-	}
-	console.log('Catalogs file exists but is older than 24 hours. Fetching catalogs...')
-	fetchCatalogs().then(() => {
-		console.log('Catalogs file updated.')
-		// process.exit(0)
+	console.log('Catalogs file exists. Fetching catalogs...')
+	fetchCatalogs()
+	cron.schedule('00 12 * * *', () => {
+		console.log('Scheduled job: Fetching catalogs...')
+		fetchCatalogs().then(() => {
+			console.log('Catalogs file updated.')
+			process.exit(0)
+		})
 	})
 } else {
 	console.log('Catalogs file does not exist. Creating file...')
 	fetchCatalogs().then(() => {
 		console.log('Catalogs file created. Fetching catalogs...')
-		fetchCatalogs().then(() => {
-			console.log('Catalogs file updated.')
-			// process.exit(0)
+		cron.schedule('00 12 * * *', () => {
+			console.log('Scheduled job: Fetching catalogs...')
+			fetchCatalogs().then(() => {
+				console.log('Catalogs file updated.')
+				process.exit(0)
+			})
 		})
 	})
 }
