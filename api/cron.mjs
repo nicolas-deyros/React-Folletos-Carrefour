a2d4d30266d948e03a0f3bc851576ff7342e14dc
Nicolas Deyros <ndeyros@Lenovo.com>
2023-04-14 11:50:50 -0300

    fix cron.mjs version 1.9.0

--- api/cron.mjs
+++ api/cron.mjs
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import { writeFile } from 'fs/promises'
 import fetch from 'node-fetch'
 import path from 'path'
@@ -13,10 +12,6 @@ const fetchCatalogs = async () => {
 		const res = await fetch('https://folletos.carrefour.com.ar/metadata/catalogs.json')
 		const data = await res.json()
 
-		if (!fs.existsSync(DATA_DIR)) {
-			fs.mkdirSync(DATA_DIR, { recursive: true })
-		}
-
 		await writeFile(DATA_FILE, JSON.stringify(data))
 		console.log(`Catalogs file saved at ${DATA_FILE}`)
 	} catch (e) {
@@ -24,31 +19,7 @@ const fetchCatalogs = async () => {
 	}
 }
 
-const fetchAndUpdateCatalogs = async () => {
-	try {
-		const now = new Date()
-		if (fs.existsSync(DATA_FILE)) {
-			const stat = fs.statSync(DATA_FILE)
-			const mtime = new Date(stat.mtime)
-			const diffHours = (now - mtime) / 1000 / 60 / 60
-			if (diffHours < 24) {
-				console.log(`Catalogs file exists and is less than 24 hours old. Skipping fetch.`)
-				return
-			}
-		}
-		await fetchCatalogs()
-	} catch (e) {
-		console.error(e)
-	}
-}
-
 // Fetch catalogs immediately
-fetchAndUpdateCatalogs()
-
-// Schedule cron job to fetch catalogs every day at midnight
-cron.schedule('0 6 * * *', async () => {
-	console.log('Scheduled job: Fetching catalogs...')
-	await fetchAndUpdateCatalogs()
-})
+fetchCatalogs()
 
 export default fetchCatalogs
