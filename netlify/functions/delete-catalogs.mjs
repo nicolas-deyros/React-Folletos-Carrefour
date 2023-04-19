eb91e03336cea7f2107eb918847aee874cd33f17
Nicolas Deyros <ndeyros@Lenovo.com>
2023-04-19 10:57:45 -0300

    add netlify

---
+++ netlify/functions/delete-catalogs.mjs
@@ -0,0 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
+const catalogsPath = new URL('../../src/data/catalogs.json', import.meta.url)
+fs.rmSync(catalogsPath)
+console.log(`Catalogs file deleted at ${path.resolve(catalogsPath.pathname)}`)
---
+++ netlify/functions/update-catalogs.mjs
@@ -0,0 +1,62 @@
+import { fetchCatalogs } from '../../src/fetch-catalogs.mjs'
+import cron from 'node-cron'
+import fs from 'fs'
+import path from 'path'
+
+const filePath = new URL('../src/data/catalogs.json', import.meta.url).pathname
+const now = new Date()
+const date = new Date()
+console.log(
+	`Catalogs fetched at ${date.toLocaleString('en-US', {
+		timeZone: 'America/Argentina/Buenos_Aires',
+	})}`
+)
+
+if (fs.existsSync(filePath)) {
+	console.log('Catalogs file exists. Fetching catalogs...')
+	fetchCatalogs()
+	let scheduledTasks = 1
+	let completedTasks = 0
+	const finish = () => {
+		completedTasks++
+		if (completedTasks === scheduledTasks) {
+			console.log('All tasks completed. Exiting process...')
+			process.exit()
+		}
+	}
+	cron.schedule('00 11 * * *', () => {
+		console.log('Scheduled job: Fetching catalogs...')
+		scheduledTasks++
+		fetchCatalogs().then(() => {
+			console.log('Catalogs file updated.')
+			finish()
+		})
+	})
+} else {
+	console.log('Catalogs file does not exist. Creating file...')
+	fetchCatalogs().then(() => {
+		console.log('Catalogs file created. Fetching catalogs...')
+		let scheduledTasks = 1
+		let completedTasks = 0
+		const finish = () => {
+			completedTasks++
+			if (completedTasks === scheduledTasks) {
+				console.log('All tasks completed. Exiting process...')
+				process.exit()
+			}
+		}
+		fetchCatalogs().then(() => {
+			console.log('Catalogs file updated.')
+			cron.schedule('00 11 * * *', () => {
+				console.log('Scheduled job: Fetching catalogs...')
+				scheduledTasks++
+				fetchCatalogs().then(() => {
+					console.log('Catalogs file updated.')
+					finish()
+				})
+			})
+			finish()
+		})
+	})
+}
+export default fetchCatalogs
